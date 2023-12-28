b4f16c01dec7f20ee70f0e4a05af469aad23e0a9
Gaurav Kedia <gaurav.kedia18@Gmail.com>
2023-12-28 16:47:17 +0530

    Update ServerSideSts1Application.java import 2

--- server_side_sts1/src/main/java/com/foreverjava/server_side_sts1/ServerSideSts1Application.java
+++ server_side_sts1/src/main/java/com/foreverjava/server_side_sts1/ServerSideSts1Application.java
@@ -1,7 +1,7 @@
 package com.foreverjava.server_side_sts1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation;
+import org.springframework.scheduling.annotation.*;
 
 @SpringBootApplication
 @EnableAsync
